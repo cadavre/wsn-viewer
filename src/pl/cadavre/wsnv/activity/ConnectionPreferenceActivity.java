67ec47f3184a6a8b5c9508330c6ddbc77519a120
cadavre <seweryn.zeman@gmail.com>
2012-11-16 01:22:08 +0100

    some semantical refactor

--- src/pl/cadavre/wsnv/activity/ConnectionPreferenceActivity.java
+++ src/pl/cadavre/wsnv/activity/ConnectionPreferenceActivity.java
@@ -15,7 +15,7 @@ public class ConnectionPreferenceActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.conn_settings);
+        setContentView(R.layout.activity_conn_pref);
 
     }
 
